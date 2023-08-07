ba8e3496b63b4ef2b7640eaa19b7df272324ff57
Murmure0 <berthet.maelle@hotmail.fr>
2023-08-07 07:17:43 +0200

    Testing translator, mini app : ok

--- appli/app.py
+++ appli/app.py
@@ -5,17 +5,23 @@ from flask import Flask, render_template, request, Response,session
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 from io import BytesIO
+from translator import myUpperCase
+# ------------------------------------------------------------------------- #
 
 app = Flask(__name__)
+
+# Session
 app.secret_key = '123'
 
+# ------------------------------------------------------------------------- #
 
 @app.route('/', methods=['GET', 'POST'])
 def translate_text():
+
     if request.method == 'POST':
         input_text = request.form['input_text']
 
-        translation_result = input_text  # TRANSLATE FUNCTION
+        translation_result = myUpperCase(input_text)  # TRANSLATE FUNCTION
 
         session['translation_result'] = translation_result
         return render_template('translate.html', translation_result=session.get('translation_result'))
@@ -35,25 +41,12 @@ def download_pdf():
 
 
 def generate_pdf_content(text):
-    # Create a buffer for the PDF content
     buffer = BytesIO()
-
-    # Create a PDF canvas
     c = canvas.Canvas(buffer, pagesize=letter)
-
-    # Set the font and font size
     c.setFont('Helvetica', 12)
-
-    # Write the text to the PDF
     c.drawString(100, 700, text)
-
-    # Save the PDF to the buffer
     c.save()
-
-    # Reset the buffer position
     buffer.seek(0)
-
-    # Return the PDF content as bytes
     return buffer.getvalue()
 
 if __name__ == '__main__':
---
+++ appli/translator.py
@@ -0,0 +1,10 @@
+from huggingface_hub import notebook_login
+
+
+
+
+
+
+
+def myUpperCase(x):
+    return x.upper()
